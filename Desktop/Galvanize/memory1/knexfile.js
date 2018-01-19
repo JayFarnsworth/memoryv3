5ed70043da0b43cca1031cf2dd77b24ee9c8a594
Jay "Danger" Farnsworth <DrDanger@Jays-MacBook-Pro.local>
2018-01-19 14:43:32 -0700

    initial

---
+++ Desktop/Galvanize/memory1/knexfile.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgresql:///memory1'
+
+  },
+
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+
+};
